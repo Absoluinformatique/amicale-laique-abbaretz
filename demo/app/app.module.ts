fc3ced46de422736c8468f11b5bedb4a1a340100
Gweltaz Niquel <gweltaz.niquel@gmail.com@users.noreply.github.com>
2021-10-27 11:31:09 +0200

    Integrate chrismas tree order form

--- demo/app/app.module.ts
+++ demo/app/app.module.ts
@@ -1,11 +1,28 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { NgxPayPalModule } from '../../projects/ngx-paypal-lib/src/public_api';
-import { AppComponent } from './app.component';
-import { HomeComponent } from './pages/home.component';
-import { TestComponent } from './pages/test.component';
-import { AppRoutesModule } from './app.routes';
+import {NgxPayPalModule} from '../../projects/ngx-paypal-lib/src/public_api';
+import {AppComponent} from './app.component';
+import {HomeComponent} from './pages/home.component';
+import {TestComponent} from './pages/test.component';
+import {AppRoutesModule} from './app.routes';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+
+// Angular material modules
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatCardModule} from '@angular/material/card';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatButtonModule} from '@angular/material/button';
+import {MatTooltipModule} from '@angular/material/tooltip';
+import {MatIconModule} from '@angular/material/icon';
+import {MatTableModule} from '@angular/material/table';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
 
 @NgModule({
   declarations: [
@@ -15,10 +32,27 @@ import { AppRoutesModule } from './app.routes';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatDividerModule,
+    MatProgressSpinnerModule,
+    MatButtonModule,
+    MatTooltipModule,
+    MatIconModule,
+    MatTableModule,
+    MatMenuModule,
+    MatCheckboxModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
     NgxPayPalModule,
-    AppRoutesModule
+    AppRoutesModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- demo/app/pages/home.component.ts
+++ demo/app/pages/home.component.ts
@@ -1,12 +1,44 @@
 import { AfterViewInit, ChangeDetectionStrategy, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 import { ICreateOrderRequest, IPayPalConfig } from '../../../projects/ngx-paypal-lib/src/public_api';
+import {FormControl, Validators} from '@angular/forms';
 
 declare var hljs: any;
+
+
+interface Classe {
+  value: string
+}
+
+interface Eleve {
+  nom: string
+  prenom: string
+  classe: Classe
+}
+
+interface Sapin {
+  value: string;
+  viewValue: string;
+  price: number;
+  quantity: number;
+}
+
+interface Buche {
+  value: string;
+  viewValue: string;
+  price: number;
+  quantity: number;
+}
+
+interface Cart {
+  sapins: Sapin[];
+  buches: Buche[];
+}
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements AfterViewInit, OnInit {
   public defaultPrice: string = '9.99';
@@ -16,6 +48,101 @@ export class HomeComponent implements AfterViewInit, OnInit {
   public showCancel: boolean = false;
   public showError: boolean = false;
   public readonly npmCode = `npm install ngx-paypal --save`;
+
+
+  email = new FormControl('', [Validators.required, Validators.email]);
+  eleve: Eleve;
+  selectedClasse: Classe;
+  selectedBuche: Buche;
+  selectedSapin: Sapin;
+  cart: Cart;
+
+  classes: Classe[] = [
+    {value: 'PS'},
+    {value: 'MS'},
+    {value: 'GS'},
+    {value: 'CP'},
+    {value: 'CE1'},
+    {value: 'CE2'},
+    {value: 'CM1'},
+    {value: 'CM2'}
+  ]
+
+  sapins: Sapin[] = [
+    {value: 'Sans - 0€', viewValue: 'Sans - 0€', price: 0, quantity: 1 },
+    {value: '125/150cm diamètre 50', viewValue: '125/150cm diamètre 50 - 20€', price: 2000, quantity: 1 },
+    {value: '150/175cm diamètre 50', viewValue: '150/175cm diamètre 50 - 25€', price: 2500, quantity: 1 },
+    {value: '175/200cm diamètre 50', viewValue: '175/200cm diamètre 50 - 30€', price: 3000, quantity: 1 }
+  ];
+  buches: Buche[] = [
+    {value: 'Sans', viewValue: 'Sans - 0€', price: 0, quantity: 1 },
+    {value: 'Diamètre 50', viewValue: 'Diamètre 50 - 4,50€', price : 450, quantity: 1 }
+  ];
+
+  constructor() {
+    this.eleve = {nom: '', prenom: '', classe: this.classes[0]}
+    this.selectedSapin = this.sapins[0]
+    this.selectedBuche = this.buches[0]
+    this.cart = {
+      sapins: [],
+      buches: []
+    }
+    this.selectedClasse = this.classes[0]
+  }
+
+  getErrorMessage() {
+    if (this.email.hasError('required')) {
+      return 'Vous devez saisir une adresse mail';
+    }
+
+    return this.email.hasError('email') ? 'Merci de saisir une adresse mail valide' : '';
+  }
+
+  getTotalPrice() {
+    let total = 0;
+    for (const sapin of this.cart.sapins) {
+      total += sapin.price * sapin.quantity;
+    }
+    for (const buche of this.cart.buches) {
+      total += buche.price * buche.quantity;
+    }
+    if (total > 0) {
+      total = total / 100;
+    }
+    return total.toFixed(2);
+  }
+
+  ajoutSapin(selectedSapin: Sapin) {
+    if (selectedSapin === this.sapins[0]) {
+      return;
+    }
+    let sapinAlreadyPresent = false;
+    for (const sapin of this.cart.sapins) {
+      if(selectedSapin === sapin) {
+        sapin.quantity += 1
+        sapinAlreadyPresent = true;
+      }
+    }
+    if (!sapinAlreadyPresent) {
+      this.cart.sapins.push(selectedSapin);
+    }
+  }
+
+  ajoutBuche(selectedBuche: Buche) {
+    if (selectedBuche === this.buches[0]) {
+      return;
+    }
+    let bucheAlreadyPresent = false;
+    for (const buche of this.cart.buches) {
+      if (this.selectedBuche === buche) {
+        buche.quantity += 1
+        bucheAlreadyPresent = true
+      }
+    }
+    if (!bucheAlreadyPresent) {
+      this.cart.buches.push(selectedBuche);
+    }
+  }
 
   public readonly moduleInstallation = `
   import { NgxPayPalModule } from 'ngx-paypal';
@@ -30,14 +157,14 @@ export class HomeComponent implements AfterViewInit, OnInit {
 
   public readonly initPaypalCode = `this.payPalConfig = {
       currency: 'EUR',
-      clientId: 'sb',
+      clientId: 'AbD6q330sVV8dEIs1u9Uwy0qUK10YNwEIfeaiqu0TRc0EbJ2RQIp2v29phESxbp05YkyLbaYpW2ZYYvk',
       createOrderOnClient: (data) => <ICreateOrderRequest>{
         intent: 'CAPTURE',
         purchase_units: [
           {
             amount: {
               currency_code: 'EUR',
-              value: '9.99',
+              value: get,
               breakdown: {
                 item_total: {
                   currency_code: 'EUR',
@@ -110,13 +237,7 @@ export class HomeComponent implements AfterViewInit, OnInit {
   }
   `;
 
-  @ViewChild('priceElem', { static: false }) priceElem?: ElementRef;
-
-  constructor() {
-  }
-
   ngOnInit(): void {
-    this.initConfig('9.99');
   }
 
   ngAfterViewInit(): void {
@@ -124,8 +245,9 @@ export class HomeComponent implements AfterViewInit, OnInit {
   }
 
   changePrice(): void {
-    if (this.priceElem) {
-      this.initConfig(this.priceElem.nativeElement.value);
+    const price = this.getTotalPrice();
+    if (price) {
+      this.initConfig(price);
     }
   }
 
